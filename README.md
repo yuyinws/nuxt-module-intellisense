dad69992088be14844f1fb5ee4d06a3e96490596
yuyinws <lyc657508366@gmail.com>
2024-04-27 12:22:34 +0800

    feat: hit on modules

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # ext-name
 
 <a href="https://marketplace.visualstudio.com/items?itemName=yuyinws.nuxt-module-intellisense" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/yuyinws.nuxt-module-intellisense.svg?color=eee&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
-
-## License
-
-[MIT](./LICENSE) License © 2022 [Anthony Fu](https://github.com/antfu)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,57 @@
-import { window } from 'vscode'
+import type { CompletionItem, ExtensionContext } from 'vscode'
+import { languages, window } from 'vscode'
+import type { WithScope } from 'ast-kit'
+import { babelParse, walkAST } from 'ast-kit'
+import type { Node } from '@babel/types'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export function activate(context: ExtensionContext) {
+  const channel = window.createOutputChannel('Nuxt Module Intellisense')
+  channel.appendLine('Nuxt Module Intellisense')
+  const provider = languages.registerCompletionItemProvider(
+    ['javascript', 'typescript'],
+    {
+      async provideCompletionItems(document, position) {
+        if (document.fileName.includes('nuxt.config')) {
+          const fileContent = document.getText()
+
+          const ast = babelParse(fileContent, '', {
+            plugins: ['typescript'],
+          })
+
+          let completionItems: CompletionItem[] = []
+
+          walkAST<WithScope<Node>>(ast, {
+            enter(node) {
+              if (node.type === 'ObjectProperty' && node.key.loc?.identifierName === 'modules' && node.value.type === 'ArrayExpression') {
+                const currentModules = node.value.elements
+
+                const findIndex = currentModules.findIndex((item) => {
+                  if (item?.loc?.start.line === position.line + 1 && item?.loc?.start.column === position.character - 1)
+                    return true
+
+                  return false
+                })
+
+                if (findIndex > -1) {
+                  completionItems = [
+                    { label: 'hello' },
+                  ]
+                }
+              }
+            },
+          })
+
+          return completionItems
+        }
+        return []
+      },
+    },
+    // 通过引号触发
+    '\'',
+    '\"',
+  )
+
+  context.subscriptions.push(provider)
 }
 
 export function deactivate() {
