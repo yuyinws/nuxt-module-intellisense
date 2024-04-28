fcbfe17b6ee74f2775f9604cee211499022d38fb
yuyinws <lyc657508366@gmail.com>
2024-04-28 10:36:44 +0800

    chore: build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'tsup'
+import { dependencies } from './package.json'
+
+const deps = Object.keys(dependencies)
 
 export default defineConfig({
   entry: [
     'src/index.ts',
   ],
   format: ['cjs'],
+  noExternal: deps,
   shims: false,
   dts: false,
   external: [
